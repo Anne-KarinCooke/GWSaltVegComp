d60ce280b1085ed05ebc4fb72e466a7a272640a7
unknown <acoo7451@2ZF4232.mcs.usyd.edu.au>
2017-03-29 13:13:52 +1100

    problem with Infil solved

--- SoilWBtest.cpp
+++ SoilWBtest.cpp
@@ -1,7 +1,5 @@
 #include <Rcpp.h>
 using namespace Rcpp;
-// SOil Water Balance Function
-
 // [[Rcpp::export]]
 double Infil(double h, double P, double alpha_i, double k, double W0){
   
@@ -60,184 +58,138 @@ double Mo(double P, double M, double Svir, double d ){
   
 }
 
-
-
-
 // [[Rcpp::export]]
-List WB_cpp(NumericVector Rain, int alpha_i=1, double I_sub = 3.0,
-                             double slope=0.001, double Zras=3000.0){
-  
-  int i;
-  int j;
-
-  
-   int time=5;
-   int deltat=6;
-   int rows=5;
-   int cols=5;
-  float timeincr = 1/deltat;  //
-  
-  // soil moisture [mm]
-  double M[rows][cols][time];
-  
-  //  plant biomass density
-  double P[rows][cols][time];
-  // //h
-  // double h[rows][cols][time];
-  
-
-  // //Svir
-  double Svir[rows][cols][time];
-  // // flux
-  // double flux[rows][cols][time];
-
-  // //h sub
-  // double h_sub[rows][cols][deltat];
-  // //P sub
-  double P_sub[rows][cols][deltat];
-  // //M sub
-  double M_sub[rows][cols][deltat];
- 
-  // double I_sub[rows][cols][deltat];
-  // //Svirsub
-  // double Svir_sub[rows][cols][deltat];
-  // // fluxsub
-  double flux_sub[rows][cols][deltat];
- 
-  double Gr_sub[rows][cols][deltat];
-  // //Mo_sub
-  double Mo_sub[rows][cols][deltat];
-  // //WU_sub
-  double WU_sub[rows][cols][deltat];
-  //
-  // // Salt leaching
-  // double L_salt[rows][cols][deltat];
-  // // //salt rise
-  // double U_salt[rows][cols][deltat];
-  //
-
-  // double Zras[rows][cols];
-  // double cn = 0.4;
-  // double Mn = 10.0;
-  // default
-  double Zr = 400.0; //mm, Grass
-  double gmax = 0.05;//Saco et al, 2013
-  double c = 0.10;//Saco et al, 2013
-  double k1 = 5.0;//Saco et al, 2013
-  double d = 0.24;//Saco et al, 2013 //fraction of plant mortality
+List SurfaceWB(double alpha_i=1.0, double cn = 0.4, double Mn =10.0, double Rain =10.0){ // Surface Balance
   
   
+  int i = 0;
+  int j= 0;
+  int t = 1;
+  int tt = 0;
+  int t_old = 0;
+  int deltat = 4.0;
   
+  float timeincr = 1/deltat;
+  
+  int rows = 2.0;
+  int cols = 2.0;
+  
+  int time = 3.0;
+  double slope = 0.001;
   double k = 12.0;//Saco et al, 2013
   double W0 = 0.2;//Saco et al, 2013
   List vegpar = (Rcpp::List::create(Rcpp::Named("k") = k,
-                                    Rcpp::Named("Zr") = Zr,
-                                    Rcpp::Named("gmax") = gmax,
-                                    Rcpp::Named("c") = c,
-                                    Rcpp::Named("k1") = k1,
-                                    Rcpp::Named("d") = d,
-                                    Rcpp::List::create(Rcpp::Named("W0") = W0)));
-  
-  // double psi_sh = -10.0;
-  // default = Medium Light Clay
-  double n = 0.418; // porosity
-  // more soil variables for evaporation & losses
-  double K_s = 3.51*10; // mm/day
-  double b = 13.48; // neurotheta LMC
-  //  double nvg = 1.089;
-  // double  avg  = 0.0591;
-  double s_fc = 0.364/n; // Field capacity
-  double psi_s_bar = -1.5E-3; // This is the bubbling pressure
-  double hb = psi_s_bar*(-1e4);
-  // double spec_y = 0.054; //Johnson 1967 says 0.05, specific yield.
-  // double h1bar = -psi_s_bar;
-  
-  List soilpar= Rcpp::List::create(Rcpp::Named("n") = n,
-                                   Rcpp::Named("b") = b,
-                                   Rcpp::Named("K_s") = K_s,
-                                   Rcpp::Named("hb") = hb,
-                                   Rcpp::Named("psi_s_bar") = psi_s_bar,
-                                   Rcpp::Named("s_fc") = s_fc);
-                              
+                                    Rcpp::Named("W0") = W0));
+  double K_s = 3.51*10.0; // cm/day
+  List soilpar= Rcpp::List::create(Rcpp::Named("K_s") = K_s);
   
   
-  double ConcConst = 0.1;
-  double CMgw = 1.0;
-  double f = 1.0;
+  // //h sub
+  double h_sub[rows][cols][deltat];
   
-  List saltpar= Rcpp::List::create(Rcpp::Named("ConcConst") = ConcConst,
-                                   Rcpp::Named("f") = f,
-                                   Rcpp::Named("CMgw") = CMgw);
+  // //qsub
+  double q_sub[rows][cols][deltat];
+  // //runonsub
+  double runon_sub[rows][cols][deltat];
+  //  Isub
+  double I_sub[rows][cols][deltat];
   
+  double h[rows][cols][time];
+  double P[rows][cols][time];
+  double In[rows][cols][time];
   
-  P[1][1][1]=10.0;
-  M[1][1][1]=10.0;
-  Svir[1][1][1]=0.0;
-  
-  
-  int t_old;
-  int t;
-  int tt;
-  
-  if(tt == 1) {
-    t_old = t-1;
-  } else {
-     t_old = tt;
-  }
-  
-  // double Rain_in;
-  // 
-  // if (Rain[t] > 0.0 & tt == 1){
-  //   double Rain_in = 10.0*Rain[t];
-  // } else {
-  //   double Rain_in = 0.0;
-  // }
+  P[0][0][0]=0.0;
+  P[0][0][1]=10.0;
+  // M[1][1][1]=10.0;
+  h[0][0][0]=10.0;
+  In[0][0][0]=0.0;
   
   
-  for (i=1; i<= rows; i++) {
+  double rn[rows][cols];
+  
+  //    for (i = 0; i< rows; i++) {
+  
+  //      for (j = 0; j< cols; j++ ){
+  
+           for (t = 0; t< time; t++){
+  
+              for (tt = 0; tt< (deltat); tt++){
     
-    for (j=1; j<= cols; j++ ){
-      
-      for (t=1; t<= time; t++){
-        
-        for (tt=1; tt<= time; tt++){
-
-          
-          // Water uptake
-          WU_sub[i][j][tt] = WU(M_sub[i][j][t_old],P[i][j][t_old], vegpar["gmax"], vegpar["k1"])*timeincr;
-          // Growth
-          Gr_sub[i][j][tt] = Gr(Svir[i][j][t_old], P[i][j][t_old], vegpar["c"], vegpar["gmax"], vegpar["k1"])*timeincr;
-          //Mortality
-          Mo_sub[i][j][tt] = Mo(P[i][j][t_old], M[i][j][t_old], Svir[i][j][t_old],vegpar["d"])*timeincr;
-          // Plant biomass balance
-          P_sub[i][j][tt] = P[i][j][t_old] + Gr_sub[i][j][tt]- Mo_sub[i][j][tt]; /// not sure if this all is ok this way or too close to R
-          
-          // Water balance before drainage
-          M_sub[i][j][tt] = M[i][j][t_old] + I_sub - WU_sub[i][j][tt];
-          
-          // Drainage/Capillary rise (vertical water flux)
-          
-          flux_sub[i][j][tt] = L_n(M_sub[i][j][tt],Zras,soilpar["n"],vegpar["Zr"],soilpar["b"],soilpar["K_s"],soilpar["psi_s_bar"]);  
-          
-          
-          // Adjustment for M including flux
-          //
-          M_sub[i][j][tt] = M_sub[i][j][tt] +  flux_sub[i][j][tt]*timeincr;
-  
-        }
-        
-        P[i][j][t] = P_sub[i][j][deltat];
-        M[i][j][t] = M_sub[i][j][deltat];
-      }
+    if(tt == 0) {
+      t_old = t-1;
+    } else {
+      t_old = tt;
     }
-  }
-  List out = Rcpp::List::create(Rcpp::Named("P") = P[rows][cols][time],
-                                Rcpp::Named("M") = M[rows][cols][time]);
+    
+    
+    // calculation of sub daily runoff and runon
+    runon_sub[i][j][tt] = rn[i][j]*q_sub[i][j][t_old];
+    q_sub[i][j][tt] = OF(h[i][j][t_old], cn, Mn, slope)*timeincr;
+    
+    
+    double Rain_in;
+    
+    if ((Rain > 0.0) & (tt == 0)){
+      Rain_in = 10.0*Rain;
+    } else {
+      Rain_in = 0.0;
+    }
+    
+    
+    
+    // calculate water depth on soil
+    h_sub[i][j][tt] =  h[i][j][t_old] + Rain_in 
+    - Infil(h[i][j][t_old], P[i][j][t_old], alpha_i, vegpar["k"], vegpar["W0"])
+    -q_sub[i][j][tt] + runon_sub[i][j][tt];
+    
+    //     // adjust infiltration rate
+        if(h_sub[i][j][tt] < (K_s*timeincr)) {
+          alpha_i = 1.0;
+        } else {
+          alpha_i = 1-(h_sub[i][j][tt] - (K_s*timeincr))/h_sub[i][j][tt];
+        }
+    // 
+        I_sub[i][j][tt] = Infil(h[i][j][t_old], P[i][j][t_old],
+                                alpha_i, vegpar["k"], vegpar["W0"])*timeincr;
+
+    // 
   
-  return out;
+  // 
+      // # Aggregating the substep results to daily values.
+
+
+      h[i][j][t] = h_sub[i][j][deltat];
+
+      double sumI = 0.0;
+
+      for(int tt = 0; tt < deltat; ++tt)
+      {
+
+        sumI += I_sub[i][j][tt];
+
+      }
+
+
+      In[i][j][t] = sumI;
+  // 
+  // //    }
+  // //      }
+  // //    }
+  // 
+  //     return(Rcpp::List::create(Rcpp::Named("I_sub") = I_sub[rows][cols][deltat],
+  //                               Rcpp::Named("I") = In[rows][cols][time],
+  //                               Rcpp::Named("P") = P[rows][cols][time],
+  //                               Rcpp::Named("h_sub") = h_sub[rows][cols][deltat]));
+  
+              }}
+  
+  return(Rcpp::List::create(Rcpp::Named("h_sub") = h_sub[rows][cols][deltat],
+                            Rcpp::Named("q_sub") = q_sub[rows][cols][deltat],
+                            Rcpp::Named("I_sub") = I_sub[rows][cols][deltat],
+                            Rcpp::Named("runon_sub") = runon_sub[rows][cols][deltat]));
   
 }
+
 /*** R
- Rain <- rep(1, 10)
- WB_cpp(Rain)
-*/
+SurfaceWB()
+  */

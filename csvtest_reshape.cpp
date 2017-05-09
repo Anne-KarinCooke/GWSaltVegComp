2a9a92e2f374abaffcdcaf5bbc1eb4f93b28341d
unknown <acoo7451@2ZF4232.mcs.usyd.edu.au>
2017-05-09 11:38:11 +1000

    matrix reshaping

--- csvtest_reshape.cpp
+++ csvtest_reshape.cpp
@@ -8,68 +8,65 @@ using namespace std;
 
 
 // [[Rcpp::export]] 
-mat write_elev() {
+mat write_elev(int rows, int cols) {
   // mat::fixed<3,9> flowdirTable;
-  arma::mat elev(10,10, fill::randu);
+  arma::mat elev(rows,cols, fill::randu);
   return elev;
   
 }
 
 // [[Rcpp::export]] 
-List bla(){
-mat B = write_elev();
+List bla( int rows, int cols){
+mat B = write_elev(rows, cols);
 //Rcpp::Rcout << "B:" << std::endl << B << std::endl;
 B.save("B.txt", arma::raw_ascii);
 system("R CMD BATCH GeoTiff.R");
 // load from disk
+
 arma::mat flowdir_new;
 flowdir_new.load("new_flowdir.txt");
+
+
 arma::mat slp_matrix_new;
-
-
-
 slp_matrix_new.load("slp_matrix.txt");
-mat slp_matrix_new_ten = reshape(slp_matrix_new, 10, 10);
-
-mat flowdir_new_ten(10,10, fill::zeros);
-
-vec vec1 = flowdir_new.row(1);
-vec vec2 = flowdir_new.row(3);
-vec vec3 = flowdir_new.row(5);
-vec vec4 = flowdir_new.row(7);
-vec vec5 = flowdir_new.row(9);
-vec vec6 = flowdir_new.row(11);
-vec vec7 = flowdir_new.row(13);
-vec vec8 = flowdir_new.row(15);
-vec vec9 = flowdir_new.row(17);
-vec vec10 = flowdir_new.row(19);
 
 
-  flowdir_new_ten(0,6::10)
-    
-flowdir_new.row(1) = 
+mat flowdir_new_ten(rows,cols, fill::zeros);
+mat slp_matrix_new_ten(rows,cols, fill::zeros);
 
-for(int i=0; i<5; i++){
+
+for(int i=0; i<(cols/2); i++){
+  flowdir_new_ten(0,(cols/2)+i) = flowdir_new(1,0+i);
+  flowdir_new_ten(0,0+i) = flowdir_new(0,0+i);
   
-  for(int j=1; j<17; j++){
+  for(int j=0; j<cols; j++){
+    for(int k=1; k<rows; k++){
+
+  
+      if(k%2!=0) {    
+    flowdir_new_ten(j,(cols/2)+i) = flowdir_new(k,0+i); //1,3,5
+
+      }
 
     
-    flowdir_new_ten(0,6+i) = flowdir_new(1,0+i);
+    if(k%2==0) {          
     
-    flowdir_new_ten(1,6+i) = flowdir_new(0,1+i);
-    
-    if ( j % 2 == 0){
-    flowdir_new_ten(j,6+i) = flowdir_new(j+1,1+i);
+    flowdir_new_ten(j,0+i) = flowdir_new(k,0+i); //2,4,6
+ 
     }
+    
+    }
+ 
+  }  
+   
  }
-}
 
- // extract a column vector
 
 
  
-List output = List::create(Rcpp::Named("slp_matrix_new_ten") = slp_matrix_new_ten,
-                           Rcpp::Named("flowdir_new_ten") =  flowdir_new_ten);
+List output = List::create(Rcpp::Named("flowdir") =  flowdir_new_ten,
+                           Rcpp::Named("slope") =  slp_matrix_new_ten);
+                           
  
 
 return output;
@@ -78,7 +75,12 @@ return output;
 
 
 /*** R
-bla()
+rows=10
+cols=10
+results <- bla(rows, cols)
+
+results$flowdir
+results$slope
 
 */
 

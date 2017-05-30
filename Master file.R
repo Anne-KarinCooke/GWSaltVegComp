e76fdb1c02b816217bfe931498d4f1cb54eb428f
Cooke <anna-karin.cooke@wur.nl>
2017-05-30 17:26:53 +0200

    also tiedied up, commented, adjusted

--- Master file.R
+++ Master file.R
@@ -3,18 +3,7 @@
 # This file sets 
 
 
-### Sourcing the grid, taudem etc, generates flowdir (ang) and slope raster
 source("Rainfall.R")
-
-### RUN TIME (how many days)
-time <- 1000
-### Raster size
-ext <- 200 ## EXTEND of PLOT in [m]
-Z <- 3000.0 ##Groundwater depth in mm from 0 elevation
-rows <- 10## rows of cells
-cols <- 10 ## columns of cells
-gslp <- 0.02
-
 # alpha <- c(0.6,1.5) # 
 alpha <- seq(0.6,1.5,by=0.1)
 lambda <- seq(0.1,1,by=0.1)
@@ -32,32 +21,6 @@ for (k in 1:length(alpha)) {
 
 Rain <- c(Rain)
 
-### Sourcing the grid, taudem etc, generates flowdir (ang) and slope raster
-# source("Rasters.R")
-# ## preparations, put them somewhere else later
-# flowdir <- ang # ang is angle flowdir[i,j] from DInf TauDEM as raster
-# flowdir[is.na(flowdir)] <- 8.0   ###********************The loop had problems with NA, so I changed NA from the blundaries to be 8. 8 is outside of 2pi...
-# flowdir <- as.matrix(flowdir,nrow= nrow(flowdir), ncol=ncol(flowdir))
-# # slope raster generated from Rasters.R transformed into matrix
-# slp_matrix<- as.matrix(slp,nrow= nrow(slp), ncol=ncol(slp))
-# Zras_matrix <- as.matrix(Zras,nrow= nrow(Zras), ncol=ncol(Zras))
-
-# write.table(flowdir, "flowdir.txt")
-# write.table(slp_matrix, "slp_matrix.txt")
-# write.table(Zras_matrix, "Zras_matrix.txt")
-
-
-# Sourcing the cpp functions that define the constants for soil, veg and salt
-# flowdir <- read.table("flowdir.txt")
-# flowdir <- as.matrix(flowdir)
-# slp_matrix <- read.table("slp_matrix.txt")
-# slp_matrix <- as.matrix(slp_matrix)
-# library(raster)
-# 
-# Zras<-raster(elev)
-# values(Zras)<-(values(elev)*1000)+Z
-# # Zras_matrix <- read.table("Zras_matrix.txt")
-# Zras_matrix <- as.matrix(Zras)
 
 sourceCpp("soilfun.cpp")
 sourceCpp("vegfun.cpp")
@@ -70,75 +33,62 @@ soilpar1 <- Soil_cpp("S Clay Loam")
 vegpar1 <-Veg_cpp("Fantasy Tree")
 saltpar1 <- Salt_cpp("Groundwater")  ## other options: "Rain", "Both", "None", "Groundwater"
 
-sourceCpp("Model_largechanges_ponding.cpp")  # dims has changed in new version
-#sourceCpp("Model_largechanges.cpp")  # dims has changed in new version
+### RUN TIME (how many days)
+time <- 5.0
+### Raster size
 
-# total <- 20
-# pb <- winProgressBar(title = "progress bar", min = 0,
-#                      max = total, width = 300)
-# 
-# for(i in 1:total){
-#   Sys.sleep(0.1)
-#   setWinProgressBar(pb, i, title=paste( round(i/total*100, 0),
-#                                         "% done"))
-# }
-#close(pb)
-#system.time(
-result <-SurfaceSoilSaltWBGRID(soilpar=soilpar1, vegpar=vegpar1,
-                                saltpar = saltpar1, dims = list(rows=rows,cols=cols,time=time, gslp=gslp, ext=ext, Z=Z),
-                                alpha_i =1.0, cn=0.01, Mn=0.04, Rain=Rain) #)
+Z <- 3000.0 ##Groundwater depth in mm from 0 elevation
+rows <- 5.0## rows of cells
+cols <- 5.0## columns of cells
 
+# "diverseInput"
+deltat <- 12 # temporal discretization, subdaily timesteps
+gslp <- 0.02 # hillslope [%]
+# infiltration
+alpha_i <- 1.0
+#Kinematic wave (runoff) paramters
+cn <-0.01
+Mn <- 0.04 # Manning's n
+# EXTEND of PLOT in [m]
+ext <- 200.0 
+# soil moisture diffusivity
+ Dm <- 0.27 #Saco and Moreno-de las Heras,2013
+# coefficient impact of plant interference
+ zeta <- 0.2
+# ecosystem carrying capacity
+P0 <-  200.0
+# sensitivity to salinity
+sigmaP <- 0.5
+# interference parameters, competition and facilitation
+ b1 <- 0.1
+ b2 <- 0.9
+ q1 <-  2.0
+ q2 <-  4.0
+# seed dispersal and diffusion
+c1 <- 2.25; # [1/mm] Saco and Moreno-de las Heras 2013
+c02 <- 0.0002 ;  #[m/d] tranformed to [mm/deltat] Saco and Moreno-de las Heras 2013
+#seed diffusivity
+Dp <- 0.3 #Saco and Moreno-de las Heras,2013
 
-qr<-brick(result$fields[[8]][2:9,2:9,50:100])
-levelplot(qr,main="P [g/m^2] ",sub="day 20 to day 40") 
+sourceCpp("Model_withPlantInterference.cpp")  
 
-
-result$fields[[11]][1:10,1:10,90:100]
-
-result$fields[[6]][1:10,1:10,300:400]
-          
-#system.time()
-#str(result$fields)
-
-Ppalette <- brewer.pal(5,"YlGn")
-
-library(rasterVis)
-qr<-brick(result$fields[[17]][2:9,2:9,10:110])
-levelplot(qr,main="P [g/m^2] ",sub="day 1 to day 100, salt from gw, randomly varied alpha and lambda",col.regions = terrain.colors(20)) 
-animate(qr, n=1)
-
-
-# f1( 0 ) = h;
-# f1( 1 ) = q;
-# f1( 2 ) = In;
-# f1( 3 ) = runon;
-# f1( 4 ) = Wu;
-# f1( 5 ) = P;
-# f1( 6 ) = flux;
-# f1( 7 ) = M;
-# f1( 8 ) = SmM;
-# f1( 9 ) = CM;
-# f1( 10 ) = mb;
-# f1( 11 ) = Svir;
-# f1( 12 ) = SmI;
-# f1( 13 ) = Smh;
-# f1( 14 ) = Ch;
-# f1( 15 ) = qsd;
-# f1( 16 ) = runonsd;
-# f1( 17 ) = seep;
-
-# #   
-#   result<- SurfaceSoilSaltWBGRID(soilpar=soil_simple(), vegpar=veg_simple(),
-#                                  saltpar = salt_simple(), dims = list(rows=10,cols=10,time=100),
-#                                  alpha_i =1.0, cn=0.01, Mn=0.04, Rain=Rain, slope=0.001,Zras=1000.0)
-#   result$fields[[1]][1:10,1:10,2]
-# #str(result$fields)
-#   result$fields[[6]]
+SurfaceSoilSaltWBGRID(soilpar=soilpar1, vegpar=vegpar1,saltpar = saltpar1, 
+                      dims = list(rows=rows,cols=cols,time=time, Z=Z),
+                      diverseInput = list (deltat = deltat, gslp = gslp, ext=ext, Dm = Dm, alpha_i = alpha_i, cn = cn, Mn = Mn, P0 = P0, sigmaP= sigmaP,
+                      c1 = c1, c02 = c02, Dp = Dp, b1 = b1, b2 = b2, q1 = q1, q2 = q2, zeta = zeta), 
+                      Rain=Rain) 
 
 
-# SurfaceSoilSaltWBGRID(alpha_i =1.0, cn=0.01, Mn=0.04, Rain=1.0, slope=0.001,Zras=1000.0, soilpar=soilpar_in, vegpar=vegpar_in,saltpar=saltpar_in) 
+### results Visualization
 
-# hdata <- as.data.frame(result$fields[1])
-# write.table(hdata, "C:/Users/acoo7451/Desktop/hdata.txt", sep="\t")
 
-# Grid_run()
+# library(rasterVis)
+# qr<-brick(result$fields[[17]][2:9,2:9,10:110])
+# levelplot(qr,main="P [g/m^2] ",sub="day 1 to day 100, salt from gw, randomly varied alpha and lambda",col.regions = terrain.colors(20)) 
+# animate(qr, n=1)
+# qr<-brick(result$fields[[8]][2:9,2:9,50:100])
+# levelplot(qr,main="P [g/m^2] ",sub="day 20 to day 40") 
+# result$fields[[11]][1:10,1:10,90:100]
+# result$fields[[6]][1:10,1:10,300:400]
+
+
